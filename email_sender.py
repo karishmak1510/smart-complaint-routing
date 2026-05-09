b528f52ae0d9450808d2655b8a46de4a57225905
karishmak1510 <karishmakri8765@email.com>
2026-05-10 01:52:20 +0530

    Improved SMTP email handling

--- email_sender.py
+++ email_sender.py
@@ -1,28 +1,59 @@
 import smtplib
+import socket
+
 from email.mime.text import MIMEText
-from config import SENDER_EMAIL, APP_PASSWORD
 
-def send_email(to_email, message, subject):
+from config import (
+    SENDER_EMAIL,
+    APP_PASSWORD
+)
 
-    msg = MIMEText(message)
+def send_email(
+    to_email,
+    message,
+    subject
+):
 
-    msg["Subject"] = subject
-    msg["From"] = SENDER_EMAIL
-    msg["To"] = to_email
+    try:
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
+        socket.setdefaulttimeout(20)
 
-    server.starttls()
+        msg = MIMEText(message)
 
-    server.login(
-        SENDER_EMAIL,
-        APP_PASSWORD
-    )
+        msg["Subject"] = subject
+        msg["From"] = SENDER_EMAIL
+        msg["To"] = to_email
 
-    server.sendmail(
-        SENDER_EMAIL,
-        to_email,
-        msg.as_string()
-    )
+        server = smtplib.SMTP(
+            "smtp.gmail.com",
+            587,
+            timeout=20
+        )
 
-    server.quit()
+        server.ehlo()
+
+        server.starttls()
+
+        server.login(
+            SENDER_EMAIL,
+            APP_PASSWORD
+        )
+
+        server.sendmail(
+            SENDER_EMAIL,
+            to_email,
+            msg.as_string()
+        )
+
+        server.quit()
+
+        print(
+            "✅ Email sent successfully"
+        )
+
+    except Exception as e:
+
+        print(
+            "❌ Email Error:",
+            e
+        )
